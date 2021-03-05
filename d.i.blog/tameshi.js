019e18b38730f017f12df0599eb50e00367cebab
Hamasuo <habocika.r34x@gmail.com>
2021-03-05 20:00:54 +0900

    2021/03/05 20:00

--- d.i.blog/tameshi.js
+++
@@ -1,7 +0,0 @@
-document.write("■■■問8■■■"+"<br>")
-    for(var i=1;i<=5;i++){
-        for(var a=1;a<=9;a++){
-            document.write(i*a);}
-       document.write()
-        }
-document.write("<br><br>")
