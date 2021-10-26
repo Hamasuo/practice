d0cf997493454e1cf664df9f22f9881a5c9ff273
Hamasuo <habocika.r34x@gmail.com>
2021-10-26 22:26:09 +0900

    2021/10/26 22:26

--- d.i.blogs/login.php
+++ d.i.blogs/login.php
@@ -3,6 +3,42 @@
 try{
 mb_internal_encoding("utf8");
 $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","root");
+} catch (PDOException $e) {
+    print('
+    <!DOCTYPE html>
+    <html lang="ja">
+        <head>
+            <meta charset="UTF-8">
+            <title>ログイン画面</title>
+            <link rel = "stylesheet" type = "text/css" href = "regist.css">
+        </head>
+
+        <body>
+
+            <header>ナビゲーションバー</header>
+            <main>
+                
+                <div class="main_container">
+                    <div class="page_name">ログイン画面</div>
+
+                    <div class="kanryo er">エラーが発生したためログイン情報を取得できません。</div>
+
+                    <div class="submit_box">
+                        <form action="login.php">
+                            <input type="submit" class="submit" value="ログイン画面へ戻る">
+                        </form>
+                    </div>
+
+                </div>
+            </main>
+
+            <footer>フッター</footer>
+
+        </body>
+    </html>
+    ');
+    die();
+}
 
 $stmt=$pdo->prepare("select * from diworks_blogs where mail=:mail");
 $stmt->bindParam(':mail', $_POST['mail']);
@@ -48,7 +84,8 @@ print('
 </html>
 ');
 die();
-}else {
+}
+else {
 print ('
 <!DOCTYPE html>
 <html lang="ja">
@@ -179,42 +216,7 @@ print ('
 die();
 }
 }
-} catch (PDOException $e) {
-    print('
-    <!DOCTYPE html>
-    <html lang="ja">
-        <head>
-            <meta charset="UTF-8">
-            <title>ログイン画面</title>
-            <link rel = "stylesheet" type = "text/css" href = "regist.css">
-        </head>
 
-        <body>
-
-            <header>ナビゲーションバー</header>
-            <main>
-                
-                <div class="main_container">
-                    <div class="page_name">ログイン画面</div>
-
-                    <div class="kanryo er">エラーが発生したためログイン情報を取得できません。</div>
-
-                    <div class="submit_box">
-                        <form action="login.php">
-                            <input type="submit" class="submit" value="ログイン画面へ戻る">
-                        </form>
-                    </div>
-
-                </div>
-            </main>
-
-            <footer>フッター</footer>
-
-        </body>
-    </html>
-    ');
-    die();
-}
 /*
 <?php
 if(isset($_POST['user'])) {
--- d.i.blogs/test.php
+++ d.i.blogs/test.php
@@ -1,3 +1,84 @@
 <?php
-echo "id=".$_POST['id'];
+
+try{
+mb_internal_encoding("utf8");
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","root");
+} catch (PDOException $e) {
+    print('
+    <!DOCTYPE html>
+    <html lang="ja">
+        <head>
+            <meta charset="UTF-8">
+            <title>ログイン画面</title>
+            <link rel = "stylesheet" type = "text/css" href = "regist.css">
+        </head>
+
+        <body>
+
+            <header>ナビゲーションバー</header>
+            <main>
+                
+                <div class="main_container">
+                    <div class="page_name">ログイン画面</div>
+
+                    <div class="kanryo er">エラーが発生したためログイン情報を取得できません。</div>
+
+                    <div class="submit_box">
+                        <form action="login.php">
+                            <input type="submit" class="submit" value="ログイン画面へ戻る">
+                        </form>
+                    </div>
+
+                </div>
+            </main>
+
+            <footer>フッター</footer>
+
+        </body>
+    </html>
+    ');
+    die();
+}
+
+$stmt=$pdo->prepare("select * from diworks_blogs where mail=:mail");
+$stmt->bindParam(':mail', $_POST['mail']);
+$stmt->execute();
+    
+if(isset($_POST['mail'])) {
+if($rows = $stmt->fetch()) {
+if(password_verify($_POST['password'], $rows["password"])) {
+    $msg = ('ログイン完了しました。');
+    $link = '<a href="d.i.blog.html">ホーム</a>';
+} else {
+    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $link = '<a href="login.php">戻る</a>';
+}
+}
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>ログイン画面</title>
+        <link rel = "stylesheet" type = "text/css" href = "regist.css">
+    </head>
+    
+    <body>
+
+        <header>ナビゲーションバー</header>
+        <main>
+
+            <div class="main_container">
+                <div class="page_name">ログイン画面</div>
+
+                <div class="kanryo"><?php echo $msg; ?></div>
+                <div class="submit_box submit"><?php echo $link; ?></div>
+            </div>
+        </main>
+
+        <footer>フッター</footer>
+    </body>
+    
+</html>
